79ab0f5d1b74212989b25206625fb744d9b80e67
Nguyenn123 <dongcoxanh1001@gmail.com>
2021-01-29 02:12:49 +0700

    add valid register

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,21 +8,33 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  myForm?: FormGroup;
+  myForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    age: new FormControl(''),
+    address: new FormControl(''),
+    email: new FormControl(''),
+    image: new FormControl(''),
+    dob: new FormControl('')
+  });
   list?: any[];
   constructor(private router: Router,
               private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      username: [' ', Validators.required],
-      password: [' ', Validators.required],
-      age: [' ', Validators.required],
-      address: [' ', Validators.required],
-      email: [' ', Validators.required],
-      image: [' ', Validators.required],
-      dob: [' ', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z]*[0-9]*$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      age: ['', [Validators.required, Validators.min(15), Validators.max(55)]],
+      address: [''],
+      email: ['', [Validators.required, Validators.email]],
+      image: [''],
+      dob: [''],
     });
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.myForm.controls[controlName].hasError(errorName);
   }
 
   register(): void{
