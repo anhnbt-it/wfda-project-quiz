ee2dd108ca1e24a9f2fdd8512a078a152b9a5e38
anhnbt-it <anhnbt.it@gmail.com>
2021-01-28 09:50:24 +0700

    Add layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizDashboardComponent } from './layout/quiz-dashboard/quiz-dashboard.component';
+import { QuizFormComponent } from './layout/quiz-form/quiz-form.component';
+import { QuizTableComponent } from './layout/quiz-table/quiz-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: QuizDashboardComponent },
+  { path: 'form', component: QuizFormComponent },
+  { path: 'table', component: QuizTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
