7e2de88f7d12e0f2d03e46b820524f2120bbf255
anhnbt-it <anhnbt.it@gmail.com>
2021-01-29 15:56:59 +0700

    Remove component test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {ExamComponent} from './views/exam/exam.component';
 import {Routes, RouterModule} from '@angular/router';
 import {QuizDashboardComponent} from './views/layout/quiz-dashboard/quiz-dashboard.component';
-import {QuizFormComponent} from './views/layout/quiz-form/quiz-form.component';
-import {QuizTableComponent} from './views/layout/quiz-table/quiz-table.component';
 import {LoginComponent} from './views/login/login.component';
 import {RegisterComponent} from './views/register/register.component';
 import {LayoutLoginComponent} from './views/layout-login.component';
@@ -19,15 +17,11 @@ const routes: Routes = [
     path: 'dashboard',
     component: QuizDashboardComponent,
   },
-  {path: 'form', component: QuizFormComponent},
-  {path: 'table', component: QuizTableComponent},
   {
     path: '',
     component: LayoutComponent,
     children: [
       {path: 'home', component: QuizDashboardComponent},
-      {path: 'form', component: QuizFormComponent},
-      {path: 'table', component: QuizTableComponent},
       {path: 'assignments-history', component: AssignmentsHistoryComponent},
       {path: 'assignments-detail', component: AssignmentsDetailComponent},
       {path: 'detail-group', component: DetailGroupComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,20 +11,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { QuizFormComponent } from './views/layout/quiz-form/quiz-form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { QuizTableComponent } from './views/layout/quiz-table/quiz-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { QuizDashboardComponent } from './views/layout/quiz-dashboard/quiz-dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
-import {RouterModule} from '@angular/router';
 import { ExamComponent } from './views/exam/exam.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { LayoutLoginComponent } from './views/layout-login.component';
@@ -40,8 +37,6 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   declarations: [
     AppComponent,
     LayoutComponent,
-    QuizFormComponent,
-    QuizTableComponent,
     QuizDashboardComponent,
     DetailGroupComponent,
     ExamComponent,
