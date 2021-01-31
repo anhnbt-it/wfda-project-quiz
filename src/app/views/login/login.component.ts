b875af108ff469d354ce6b59f648fc4475e5c134
Nguyenn123 <dongcoxanh1001@gmail.com>
2021-02-01 01:08:27 +0700

    save token => localStorage

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Route, Router} from '@angular/router';
 import {UserService} from '../../containers/service/user/user.service';
+import {LocalStorageService} from "../../containers/service/localStorage.service";
 
 @Component({
   selector: 'app-login',
@@ -13,13 +14,15 @@ export class LoginComponent implements OnInit {
     password: ''
   };
   constructor(private router: Router,
-              private userService: UserService) { }
+              private userService: UserService,
+              private localStorage: LocalStorageService) { }
 
   ngOnInit(): void {
   }
 
   login(): void {
     this.userService.login(this.user).subscribe(res => {
+      this.localStorage.set('user', res.accessToken);
       console.log(res);
       this.gotoHome();
     });
